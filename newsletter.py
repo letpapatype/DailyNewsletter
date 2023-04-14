faeaf1e939339cd9cb13e204823fa6adb5b1fec6
letpapatype <82685246+letpapatype@users.noreply.github.com>
2023-04-14 18:24:45 -0400

    First commit
    https://docs.google.com/document/d/12lkNEVw1Ca2jQpbf3NTcCJlAGHfpkI4gqElJtQ8HDTk/edit

---
+++ newsletter.py
@@ -0,0 +1,50 @@
+import os
+from datetime import date
+from sendgrid import SendGridAPIClient
+from sendgrid.helpers.mail import Mail
+import yfinance as yf
+
+
+def stockPrices():
+  stocks = ['aapl','sbux','amzn','aos','cost','dhi','googl','iyt','low','panw','snps','uber','voo']
+  results = []
+  for stock in stocks:
+    price = yf.Ticker(f"{stock}").info
+    results.append(f"{stock.upper()} closed at {price['previousClose']} after opening at {price['open']}")
+
+  return results
+
+# generate todays date for the email subject line
+today = date.today()
+todays_date = today.strftime("%B %d, %Y")
+stockprices = []
+
+message = Mail(
+    from_email = 'jovan@jovannewland.net',
+    to_emails= 'jovannewland@gmail.com',
+    subject = f"Daily Digest for {todays_date}",
+    html_content=f"<strong>Good Afternoon Jovan. Here's what's going on today</strong> \
+    <p>Here's how some of your favorite stocks opened the day:<br> \
+    {stockPrices()[0]}<br> \
+    {stockPrices()[1]}<br> \
+    {stockPrices()[2]}<br> \
+    {stockPrices()[3]}<br> \
+    {stockPrices()[4]}<br> \
+    {stockPrices()[5]}<br> \
+    {stockPrices()[6]}<br> \
+    {stockPrices()[7]}<br> \
+    {stockPrices()[8]}<br> \
+    {stockPrices()[9]}<br> \
+    {stockPrices()[10]}<br> \
+    {stockPrices()[11]}<br> \
+    {stockPrices()[12]}"
+)
+
+try:
+    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+    response = sg.send(message)
+    print(response.status_code)
+    print(response.body)
+    print(response.headers)
+except Exception as e:
+    print(e.message)
