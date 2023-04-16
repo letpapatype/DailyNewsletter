67a181f818d9ab6203a1c81323e99051f59d494c
letpapatype <82685246+letpapatype@users.noreply.github.com>
2023-04-15 17:32:01 -0700

    Price iteration clean up and reqs

--- newsletter.py
+++ newsletter.py
@@ -1,8 +1,9 @@
 import os
 from datetime import date
 from sendgrid import SendGridAPIClient
-from sendgrid.helpers.mail import Mail
+from sendgrid.helpers.mail import Mail, Content
 import yfinance as yf
+from jinja2 import Template
 
 
 def stockPrices():
@@ -13,27 +14,41 @@ def stockPrices():
     results.append(f"{stock.upper()} closed at {price['previousClose']} after opening at {price['open']}")
 
   return results
+def sendEmail():
+  # generate todays date for the email subject line
+  today = date.today()
+  todays_date = today.strftime("%B %d, %Y")
+  prices = stockPrices()
+  template = Template("""
+      <font color="black"><strong>Good Afternoon Jovan. Here's what's going on today</strong> \
+      <p>Here's how some of your favorite stocks opened the day:<br><ul>
+      {% for price in prices %}
+        <li>{{ price }}</li>
+      {% endfor %}
+      </ul></p></font>
+  """)
 
-# generate todays date for the email subject line
-today = date.today()
-todays_date = today.strftime("%B %d, %Y")
-stockprices = []
+  html_content = template.render(prices=prices)
+  content = Content("text/html", html_content)
+  message = Mail(
+      from_email = 'jovan@jovannewland.net',
+      to_emails= 'jovannewland@gmail.com',
+      subject = f"Daily Digest for {todays_date}",
+      html_content = content
+  )
 
-message = Mail(
-    from_email = 'jovan@jovannewland.net',
-    to_emails= 'jovannewland@gmail.com',
-    subject = f"Daily Digest for {todays_date}",
-    html_content=f"<strong>Good Afternoon Jovan. Here's what's going on today</strong> \
-    <p>Here's how some of your favorite stocks opened the day:<br> \
-    {stockPrices()[0]}<br> {stockPrices()[1]}<br> {stockPrices()[2]}<br> {stockPrices()[3]}<br> {stockPrices()[4]}<br> {stockPrices()[5]}<br> \
-    {stockPrices()[6]}<br> {stockPrices()[7]}<br> {stockPrices()[8]}<br> {stockPrices()[9]}<br> {stockPrices()[10]}<br> {stockPrices()[11]}<br> {stockPrices()[12]}</p>"
-)
+  # for stock_update in prices:
+  #   message.add_content(str(f"<li>{stock_update}</li><br>"), mime_type='text/html')
 
-try:
-    sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
-    response = sg.send(message)
-    print(response.status_code)
-    print(response.body)
-    print(response.headers)
-except Exception as e:
-    print(e.message)
+
+
+  try:
+      sg = SendGridAPIClient(os.environ.get('SENDGRID_API_KEY'))
+      response = sg.send(message)
+      print(response.status_code)
+      print(response.body)
+      print(response.headers)
+  except Exception as e:
+      print(e.message)
+
+sendEmail()
